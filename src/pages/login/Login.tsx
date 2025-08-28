524809962d9ade05b777824894d8715aed218f74
Matheus97px <matheus97p.q@gmail.com>
2025-08-28 20:34:08 -0300

    feat: confirmação login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,10 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Home from "../home/Home";
 
 function Login() {
 
+  const [completed, setCompleted] = useState(false);
   const [isLogged, setIsLogged] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const [login, setLogin] = useState('');
 
+
+   useEffect(() => {
+      if (completed) {
+        setLogin('Logar');
+      } 
+      else {
+        setLogin('Confirme antes de Logar');
+      }
+      
+    
+  
+    }, [completed])
+  
 
   return (
     <>
@@ -12,7 +28,17 @@ function Login() {
       {isLogged ? (<Home />) : (
         <div>
           <h2>Componente Login</h2>
-          <button onClick={() => setIsLogged(true)}>Entrar</button>
+          <label>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={(e) => {setIsChecked(e.target.checked); setCompleted(e.target.checked)}} 
+              
+            />
+            Confirmo que desejo Logar
+          </label>
+          <br />
+          <button onClick={() => !isChecked ? alert('confirme primeiro antes de entrar') : setIsLogged(true)} disabled={!isChecked}>{login}</button>
         </div>
       )}
       
